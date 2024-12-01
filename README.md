efa74a9f270f7ab451a90300e4b9a0e80db04fc9
Mark Seliverstov <smdmrr00@gmail.com>
2024-12-01 21:51:53 +0100

    chore: init AoC

---
+++ README.md
@@ -0,0 +1,11 @@
+# AoC Solution 2024
+
+## How to use?
+
+```bash
+python <dir> # to get results from input.txt\n
+python <dir> test <first_result> <second_result> # to test script
+
+#Example:
+python 1 test 11 35
+```
---
+++ template/solution.py
@@ -0,0 +1,9 @@
+from typing import Iterable
+
+
+def first_part_solution(input: Iterable[str]) -> int:
+    raise NotImplementedError("First part is not implemented yet")
+
+
+def second_part_solution(input: Iterable[str]) -> int:
+    raise NotImplementedError("Second part is not implemented yet")
