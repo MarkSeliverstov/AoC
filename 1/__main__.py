e047a6f11af06e9c7ce1b6d4cd0c8dae2bf67824
Mark Seliverstov <smdmrr00@gmail.com>
2024-12-01 21:53:06 +0100

    chore: add 1 solution

---
+++ 1/__main__.py
@@ -0,0 +1,58 @@
+import sys
+from collections.abc import Iterator
+from pathlib import Path
+
+from solution import first_part_solution, second_part_solution
+
+
+def read_file_generator(file_name: str = "input.txt") -> Iterator[str]:
+    with open(Path(__file__).parent / file_name) as file:
+        for line in file:
+            yield line
+
+
+def test_simple_input_data_generator() -> Iterator[str]:
+    input: str = """"""
+    for row in input.splitlines():
+        yield row
+
+
+USAGE_MESSAGE: str = (
+    "Usage:\t python <dir> - to get results from input.txt\n"
+    + "\t python <dir> test <first_result> <second_result> - to test script"
+    + "\nExample: python 1 test 11 35"
+)
+
+
+def start():
+    if len(sys.argv) == 1:
+        first_result = first_part_solution(read_file_generator())
+        print(f"{first_result=}")
+        second_result = second_part_solution(read_file_generator())
+        print(f"{second_result=}")
+        return
+
+    if sys.argv[1] == "test":
+        if len(sys.argv) < 3:
+            print(USAGE_MESSAGE)
+            return
+
+        if len(sys.argv) >= 3:
+            first_expected_result: int = int(sys.argv[2])
+            first_result: int = first_part_solution(test_simple_input_data_generator())
+            assert first_result == first_expected_result, first_result
+            print(f"First part test passed: {first_result=}")
+
+        if len(sys.argv) == 4:
+            second_expected_result: int = int(sys.argv[3])
+            second_result: int = second_part_solution(
+                test_simple_input_data_generator()
+            )
+            assert second_result == second_expected_result, second_result
+            print(f"Second part test passed: {second_result=}")
+    else:
+        print(USAGE_MESSAGE)
+
+
+if __name__ == "__main__":
+    start()
---
+++ 1/solution.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+from typing import Iterable
+
+
+def first_part_solution(input: Iterable[str]) -> int:
+    left: list[int] = []
+    right: list[int] = []
+
+    for row in input:
+        splitted_line: list[str] = row.split()
+        if len(splitted_line) == 0:
+            continue
+
+        if len(splitted_line) != 2:
+            raise ValueError(f"Incorrect input: {row=}, {splitted_line=}")
+        left.append(int(splitted_line[0]))
+        right.append(int(splitted_line[1]))
+
+    left.sort()
+    right.sort()
+    assert len(left) == len(right)
+    result: int = 0
+    for l, r in zip(left, right):
+        result += abs(l - r)
+
+    return result
+
+
+def second_part_solution(input: Iterable[str]) -> int:
+    left: list[int] = []
+    right_frequency: defaultdict[int, int] = defaultdict(int)
+
+    for row in input:
+        splitted_line: list[str] = row.split()
+        if len(splitted_line) == 0:
+            continue
+
+        if len(splitted_line) != 2:
+            raise ValueError(f"Incorrect input: {row=}, {splitted_line=}")
+        left.append(int(splitted_line[0]))
+        right_frequency[int(splitted_line[1])] += 1
+
+    left.sort()
+    result: int = 0
+    for item in left:
+        result += item * right_frequency[item]
+
+    return result
