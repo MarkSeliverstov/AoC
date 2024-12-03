3f8f41ae7bd0005ae6f94a6c0991c52b10f15149
Mark Seliverstov <smdmrr00@gmail.com>
2024-12-03 09:53:04 +0100

    fix: arg name

--- template/solution.py
+++ template/solution.py
@@ -4,9 +4,9 @@ EXPECTED_RESULT_PART1: int = 0
 EXPECTED_RESULT_PART2: int = 0
 
 
-def part1(input: Iterable[str]) -> int:
+def part1(input_lines: Iterable[str]) -> int:
     raise NotImplementedError("First part is not implemented yet")
 
 
-def part2(input: Iterable[str]) -> int:
+def part2(input_lines: Iterable[str]) -> int:
     raise NotImplementedError("Second part is not implemented yet")
