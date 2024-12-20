e467384ca86ed72fe615e9f84d2554594b9a15e1
Mark Seliverstov <smdmrr00@gmail.com>
2024-12-20 09:53:08 +0100

    chore: updatre template

--- template/__main__.py
+++ template/__main__.py
@@ -7,14 +7,14 @@ def test():
     INPUT_EXAMPLE_PATH: Path = Path(__file__).parent / "input_example.txt"
     INPUT_PATH: Path = Path(__file__).parent / "input.txt"
 
-    result: int = part1(line for line in open(INPUT_EXAMPLE_PATH))
+    result: int = part1(open(INPUT_EXAMPLE_PATH).read())
     assert result == EXPECTED_RESULT_PART1, result
-    result: int = part1(line for line in open(INPUT_PATH))
+    result: int = part1(open(INPUT_PATH).read())
     print(f"Result of part 1 is {result}")
 
-    result: int = part2(line for line in open(INPUT_EXAMPLE_PATH))
+    result: int = part2(open(INPUT_EXAMPLE_PATH).read())
     assert result == EXPECTED_RESULT_PART2, result
-    result: int = part2(line for line in open(INPUT_PATH))
+    result: int = part2(open(INPUT_PATH).read())
     print(f"Result of part 2 is {result}")
 
 
--- template/solution.py
+++ template/solution.py
@@ -1,12 +1,13 @@
-from typing import Iterable
+from __future__ import annotations
+from dataclasses import dataclass
 
 EXPECTED_RESULT_PART1: int = 0
 EXPECTED_RESULT_PART2: int = 0
 
 
-def part1(input_lines: Iterable[str]) -> int:
+def part1(input: str) -> int:
     raise NotImplementedError("First part is not implemented yet")
 
 
-def part2(input_lines: Iterable[str]) -> int:
+def part2(input: str) -> int:
     raise NotImplementedError("Second part is not implemented yet")
